cd59beb630cf682dc59a7bfc264cba67ff496af9
konir <koniroth@gmail.com>
2014-03-19 22:37:56 +0100

    icons

--- platforms/android/ant-build/FeiertagRechner-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/Koni/Projects/workspace-Feiertage/Feiertag-Rechner/platforms/android/ant-build/FeiertagRechner-debug-unaligned.apk : \
-/Users/Koni/Projects/workspace-Feiertage/Feiertag-Rechner/platforms/android/ant-build/FeiertagRechner.ap_ \
-/Users/Koni/Projects/workspace-Feiertage/Feiertag-Rechner/platforms/android/ant-build/classes.dex \
-/Users/Koni/Projects/workspace-Feiertage/Feiertag-Rechner/platforms/android/CordovaLib/ant-build/classes.jar \
--- platforms/android/ant-gen/feiertage/koni/mobi/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package feiertage.koni.mobi;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
